0d6f19cca8bd53bfc4eb987b53add219db71897e
Tim Wingfield <tim@timwingfield.com>
2010-07-10 00:19:41 -0400

    finished adult pricing

---
+++ lib/magic_your_way.rb
@@ -0,0 +1,64 @@
+class MagicYourWay
+  attr_accessor :base_price
+  attr_accessor :price
+  attr_accessor :no_of_days
+  
+  attr_reader :no_expire
+  attr_reader :hopper_price
+  attr_reader :water_park_add_on_price
+  
+  def initialize
+    @hopper_price = 52
+    @water_park_add_on_price = 52
+    @no_expire = {
+      "1" => 0,
+      "2" => 18,
+      "3" => 24,
+      "4" => 52,
+      "5" => 73,
+      "6" => 84,
+      "7" => 115,
+      "8" => 152,
+      "9" => 178,
+      "10" => 209
+    }
+  end
+  
+  def adult
+    @base_price = {
+      "1" => 79,
+      "2" => 156,
+      "3" => 219,
+      "4" => 225,
+      "5" => 228,
+      "6" => 231,
+      "7" => 234,
+      "8" => 237,
+      "9" => 240,
+      "10" => 243
+    }
+    
+    self
+  end
+  
+  def days(no_of_days)
+    @no_of_days = no_of_days
+    self.price = base_price[no_of_days.to_s]
+    self
+  end
+  
+  def with_no_expire
+    self.price += no_expire[no_of_days.to_s]
+    self
+  end
+  
+  def park_hopper
+    self.price += hopper_price
+    self
+  end
+
+  def water_park_fun_and_more
+    self.price += water_park_add_on_price
+    self
+  end
+end
---
+++ spec/magic_your_way_spec.rb
@@ -0,0 +1,61 @@
+require File.dirname(__FILE__) + '/spec_helper'
+
+describe MagicYourWay do
+  before :each do
+    @myw = MagicYourWay.new
+  end
+  
+  describe "and getting adult base ticket prices" do
+   it "should return 79 for a one day ticket" do
+     @myw.adult.days(1).price.should be(79)
+   end
+   
+   it "should return 228 for a 5 day ticket" do
+     @myw.adult.days(5).price.should be(228)
+   end
+  end
+  
+  describe "and getting adult park hopper ticket prices" do
+    it "should return 277 for a 4 day park hopper" do
+      @myw.adult.days(4).park_hopper.price.should be(277)
+    end
+    
+    it "should return 295 for a 10 day park hopper" do
+      @myw.adult.days(10).park_hopper.price.should be(295)
+    end
+  end
+  
+  describe "and getting the water park add on" do
+    it "should retun 208 for a 2 day water park fun and more ticket" do
+      @myw.adult.days(2).water_park_fun_and_more.price.should be(208)
+    end
+    
+    it "should return 289 for an 8 day water park fun and more ticket" do      
+      @myw.adult.days(8).water_park_fun_and_more.price.should be(289)
+    end
+  end
+  
+  describe "and getting a hopper and water park option" do
+    it "should return 323 for a 3 day ticket" do
+      @myw.adult.days(3).park_hopper.water_park_fun_and_more.price.should be(323)
+    end
+    
+    it "should 335 for a 6 day ticket" do
+      @myw.adult.days(6).park_hopper.water_park_fun_and_more.price.should be(335)
+    end
+  end
+  
+  describe "and getting the no expiration option on a base ticket" do
+    it "should return 452 for a 10 day ticket" do
+      @myw.adult.days(10).with_no_expire.price.should be(452)
+    end
+    
+    it "should return 301 for a 5 day ticket" do
+      @myw.adult.days(5).with_no_expire.price.should be(301)
+    end
+    
+    it "should return 174 for a 2 day ticket" do
+      @myw.adult.days(2).with_no_expire.price.should be(174)
+    end
+  end
+end
